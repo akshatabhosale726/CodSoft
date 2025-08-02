dbd21a52b85718016d883442411a4a80fba2bd9d
Akshata Bhosale <bhosaleakshata726@gmail.com>
2025-08-02 12:19:15 +0530

    Add files via upload

---
+++ StudentGradeCalMain.java
@@ -0,0 +1,11 @@
+package com.codSoft;
+
+public class StudentGradeCalMain {
+	public static void main(String[] args) {
+        StudentGradeCalculator gc = new StudentGradeCalculator();
+        gc.Calculate();
+        gc.displayResult();
+    }
+}
+
+
